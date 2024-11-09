fe71e74a74aabb970cb0302f577d7d78cabe3126
Noah Hdez <legolasbat@gmail.com>
2024-11-09 14:45:07 +0000

    Spotlight

--- Shaders/shader.fs
+++ Shaders/shader.fs
@@ -15,7 +15,9 @@ uniform Material material;
 
 struct Light {
 	vec3 position;
-	//vec3 direction;
+	vec3 direction;
+	float cutOff;
+	float outerCutOff;
 
 	vec3 ambient;
 	vec3 diffuse;
@@ -30,8 +32,6 @@ uniform Light light;
 uniform vec3 viewPos;
 
 void main() {
-	float distance = length(light.position - FragPos);
-	float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * distance * distance);
 
 	// Ambient
 	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
@@ -48,7 +48,16 @@ void main() {
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
 	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
 
-	vec3 result = ambient + diffuse + specular;
+	// Spotlight
+	float theta = dot(lightDir, normalize(-light.direction));
+	float epsilon = light.cutOff - light.outerCutOff;
+	float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+
+	// attenuation
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+
+	vec3 result = ambient + (diffuse + specular) * intensity;
 	result *= attenuation;
-	FragColor = vec4(result, 1.0);
+	FragColor = vec4(result, 1.0);	
 }
